ffa74f2f6a579f8d25e4114c4f1bc46cb6ea6f0e
kwrnya <y21kurnia@gmail.com>
2020-12-10 13:40:39 +0700

    adding hit effect & enemy AI

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,12 +2,48 @@ extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
+export var acceleration = 300
+export var max_speed = 50
+export var friction = 200
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
 var knockback = Vector2.ZERO
+var velocity = Vector2.ZERO
+var state = CHASE
 onready var stats = $Stats
+onready var playerDetectionZone = $PlayerDetectionZone
+onready var sprite = $AnimatedSprite
 
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	knockback = knockback.move_toward(Vector2.ZERO, friction * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+			seek_player()
+
+		WANDER:
+			pass
+
+		CHASE:
+			var player = playerDetectionZone.player
+			if player != null:
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
+			else:
+				state = IDLE
+			sprite.flip_h = velocity.x < 0
+
+	velocity = move_and_slide(velocity)
+
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = CHASE
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage

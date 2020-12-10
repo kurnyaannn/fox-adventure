fac9ca2eac5b2c82dc8e71eb09debb633f23c1df
kwrnya <y21kurnia@gmail.com>
2020-12-10 08:30:31 +0700

    init

---
+++ Player/Player.gd
@@ -0,0 +1,75 @@
+extends KinematicBody2D
+
+const acceleration = 500
+const friction = 500
+const max_speed = 80
+const roll_speed = 90
+
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var velocity = Vector2.ZERO
+var state = MOVE
+var roll_vector = Vector2.DOWN
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+func _ready():
+	animationTree.active = true
+
+func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			roll_state(delta)
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	
+	velocity = move_and_slide(velocity)
+
+	if Input.is_action_just_pressed("Roll"):
+		state = ROLL
+	
+	if Input.is_action_just_pressed("Attack"):
+		state = ATTACK
+	
+
+func roll_state(delta):
+	velocity = roll_vector * roll_speed
+	animationState.travel("Roll")
+	move()
+
+func attack_state(delta):
+	animationState.travel("Attack")
+	
+func move():
+	velocity = move_and_slide(velocity)
+
+func roll_animation_finished():
+	state = MOVE
+
+func attack_animation_finished():
+	state = MOVE
